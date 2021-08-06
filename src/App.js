646c5235f4567acc1c0e4cc8d9cfb7d23d1ee726
Asylbek uulu Erzhan <erzhanasylbekuulu@gmail.com>
2021-08-06 20:04:42 +0300

    created some info on page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
-import './App.css';
+const styles = {
+  main: {
+    fontSize: 40,
+    color: "red"
+  },
+  text: {
+    fontSize: 25,
+    color: "green"
+  }
+}
 
 function App() {
   return (
-    
+    <div>
+      <h1 style={styles.main}>My page</h1>
+      <p style={styles.text}>My name is Erzhan and i'm frontend developer</p>
+    </div>
   );
 }
 
